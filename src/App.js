6cefbb923242f043587a95635f14c824e571b1e9
Samrridh Parashar <samrridhparashar@MacBook-Air.local>
2023-04-15 00:23:02 +0530

    Second Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import SortingVisualizer from "./Components/SortingVisualizer/SortingVisualizer";
+
+function App() {
+  return (
+    <div>
+      <SortingVisualizer />
+    </div>
+  );
+}
+
+export default App;
